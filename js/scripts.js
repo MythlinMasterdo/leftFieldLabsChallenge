19617074b23a441536ca3c3f4eb4560c1c635dee
MythlinMasterdo <miles71397@gmail.com>
2017-08-19 16:46:38 -0400

    build out basic sidebar options

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 function toggleSidebar() {
   var isClosed = document.getElementsByClassName('sidebar-closed');
-  console.log('isClosed ', isClosed);
+  // console.log('isClosed ', isClosed);
   if(isClosed.length > 0) {
     isClosed[0].setAttribute('class', 'sidebar-open');
+    document.getElementsByClassName('sidebar-open')[0].focus();
   } else {
-    document.getElementsByClassName('sidebar-open')[0].setAttribute('class', 'sidebar-closed');
+    // document.getElementsByClassName('sidebar-open')[0].setAttribute('class', 'sidebar-closed');
   }
 }
