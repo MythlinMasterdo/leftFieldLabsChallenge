99b9356ad857742b6e43edd85209f17f828ba234
MythlinMasterdo <miles71397@gmail.com>
2017-08-19 20:15:54 -0400

    toggle green for why android switch on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 function toggleSidebar() {
   var isClosed = document.getElementsByClassName('sidebar-closed');
-  // console.log('isClosed ', isClosed);
   if(isClosed.length > 0) {
     isClosed[0].setAttribute('class', 'sidebar-open');
     document.getElementsByClassName('sidebar-open')[0].focus();
   } else {
     // document.getElementsByClassName('sidebar-open')[0].setAttribute('class', 'sidebar-closed');
+  }
+}
+
+function toggleWhyAndroid() {
+  var isClosed = document.getElementsByClassName('why-android-list-closed');
+  if(isClosed.length > 0) {
+    isClosed[0].setAttribute('class', 'why-android-list-open');
+    document.getElementsByClassName('why-android-expand')[0].setAttribute('class', 'fa fa-caret-up fa-3x why-android-expand-open');
+    document.getElementsByClassName('menu-list-label')[0].setAttribute('class', 'menu-list-label-active');
+    document.getElementsByClassName('why-android-list-open')[0].focus();
+  } else {
+    document.getElementsByClassName('why-android-list-open')[0].setAttribute('class', 'why-android-list-closed');
+    document.getElementsByClassName('menu-list-label')[0].setAttribute('class', 'menu-list-label');
+    document.getElementsByClassName('why-android-expand-open')[0].setAttribute('class', 'fa fa-caret-down fa-3x why-android-expand');
   }
 }
