8b5d2b7a0c55d2f28db3ac251c36f1b718943b2a
MythlinMasterdo <miles71397@gmail.com>
2017-08-20 08:59:25 -0400

    add android favicon

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,12 @@ function toggleWhyAndroid() {
   var isClosed = document.getElementsByClassName('why-android-list-closed');
   if(isClosed.length > 0) {
     isClosed[0].setAttribute('class', 'why-android-list-open');
-    document.getElementsByClassName('why-android-expand')[0].setAttribute('class', 'fa fa-caret-up why-android-expand-open');
+    document.getElementsByClassName('why-android-expand-closed')[0].setAttribute('class', 'fa fa-caret-up why-android-expand-open');
     document.getElementsByClassName('menu-list-label')[0].setAttribute('class', 'menu-list-label-active');
     document.getElementsByClassName('why-android-list-open')[0].focus();
   } else {
     document.getElementsByClassName('why-android-list-open')[0].setAttribute('class', 'why-android-list-closed');
     document.getElementsByClassName('menu-list-label-active')[0].setAttribute('class', 'menu-list-label');
-    document.getElementsByClassName('why-android-expand-open')[0].setAttribute('class', 'fa fa-caret-down why-android-expand');
+    document.getElementsByClassName('why-android-expand-open')[0].setAttribute('class', 'fa fa-caret-down why-android-expand-closed');
   }
 }
