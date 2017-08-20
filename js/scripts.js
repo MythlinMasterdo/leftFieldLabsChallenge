d57132c2ffda1960cd58b049de3bbdfb14e3077d
MythlinMasterdo <miles71397@gmail.com>
2017-08-20 14:29:57 -0400

    add css transition to why android and remove most font awesome icons

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,24 @@ function toggleWhyAndroid() {
   var isClosed = document.getElementsByClassName('why-android-list-closed');
   if(isClosed.length > 0) {
     isClosed[0].setAttribute('class', 'why-android-list-open');
-    document.getElementsByClassName('why-android-expand-closed')[0].setAttribute('class', 'fa fa-caret-up why-android-expand-open');
+    document.getElementsByClassName('caret-down')[0].setAttribute('class', 'why-android-expand-open caret-up');
     document.getElementsByClassName('menu-list-label')[0].setAttribute('class', 'menu-list-label-active');
     document.getElementsByClassName('why-android-list-open')[0].focus();
   } else {
     document.getElementsByClassName('why-android-list-open')[0].setAttribute('class', 'why-android-list-closed');
     document.getElementsByClassName('menu-list-label-active')[0].setAttribute('class', 'menu-list-label');
-    document.getElementsByClassName('why-android-expand-open')[0].setAttribute('class', 'fa fa-caret-down why-android-expand-closed');
+    document.getElementsByClassName('caret-up')[0].setAttribute('class', 'why-android-expand-open caret-down');
   }
+  // document.getElementsByClassName('why-android-list-closed')[0].style.cssText = 'opacity: 1; height: auto;';
+  // document.getElementsByClassName('caret-down')[0].setAttribute('class', 'why-android-expand-open caret-up');
+}
+
+function addGreen(className) {
+  document.getElementsByClassName(className)[0].children[0].children[0].style.cssText = 'fill: #6ab344;';
+  document.getElementsByClassName(className)[0].children[1].style.cssText = 'color: #6ab344;';
+}
+
+function removeGreen (className) {
+  document.getElementsByClassName(className)[0].children[0].children[0].style.cssText = '';
+  document.getElementsByClassName(className)[0].children[1].style.cssText = '';
 }
